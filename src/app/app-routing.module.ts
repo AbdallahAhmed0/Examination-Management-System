3c87658321e38076bcdd31d02caab4714aedfd20
AbdallahAhmed0 <abdallahahmadadel@gmail.com>
2023-03-20 23:26:05 +0200

    add lazy loading question

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,8 @@ const routes: Routes = [
   {path:"admins",loadChildren:()=>import('./admins/admins.module').then(a=>a.AdminsModule)},
   {path:"roles",loadChildren:()=>import('./roles/roles.module').then(m=>m.RolesModule)},
   {path:"students",loadChildren:()=>import('./students/students.module').then(m=>m.StudentsModule)},
-  {path:"exams",loadChildren:()=>import('./exam/exam.module').then(m=>m.ExamModule)}
+  {path:"exams",loadChildren:()=>import('./exam/exam.module').then(m=>m.ExamModule)},
+  {path:"save",loadChildren:()=>import('./question/question.module').then(m=>m.QuestionModule)}
 
 ];
 
