c0ce875116df0ba48123400bf6cfba138c3ca1fd
Mohamed <khedr66664@gmail.com>
2023-03-26 01:31:01 +0200

    add attempt btn functionality

--- src/app/exam/Components/attempt-exam/attempt-exam.component.ts
+++ src/app/exam/Components/attempt-exam/attempt-exam.component.ts
@@ -6,25 +6,38 @@ import { ExamService } from '../../Services/exam.service';
 @Component({
   selector: 'app-attempt-exam',
   templateUrl: './attempt-exam.component.html',
-  styleUrls: ['./attempt-exam.component.scss']
+  styleUrls: ['./attempt-exam.component.scss'],
 })
 export class AttemptExamComponent implements OnInit {
-
-  constructor(private _examService:ExamService, private _activatedRoute:ActivatedRoute) {}
+  constructor(
+    private _examService: ExamService,
+    private _activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.attemptExam();
+    this.getExamInfo();
   }
 
   examInfo!: Exam;
+  examId!: number;
 
-  attemptExam(){
-    let examId = Number(this._activatedRoute.snapshot.paramMap.get('examId')) ;
-    this._examService.getExamById(examId).subscribe(data =>{
-      this.examInfo = data 
+  getExamInfo() {
+    this.examId = Number(this._activatedRoute.snapshot.paramMap.get('examId'));
+    this._examService.getExamById(this.examId).subscribe((data) => {
+      this.examInfo = data;
       // err => throwError(err || "an error happened while getting exam info")
-  });
+    });
   }
 
+  // Send userId and examId then get response with user data
+  getAttemptExamData(userId: number) {
+    this._examService
+      .attemptExam(this.examId, userId)
+      .subscribe((data) => console.log(data));
+  }
 
+  attemptExam() {
+    // this.router.navigate();   //This should take the user to the exam
+    this.getAttemptExamData(12); //FIXed userID
+  }
 }
