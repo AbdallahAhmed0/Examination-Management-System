05667796f5930d1f28ecf31045ea3df25840ec8e
AbdallahAhmed0 <abdallahahmadadel@gmail.com>
2023-04-11 14:51:05 +0200

    edit  import admins

--- src/app/admins/Components/import-admin/import-admin.component.ts
+++ src/app/admins/Components/import-admin/import-admin.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Admin } from '../../Models/admin';
-import { read, utils, writeFile } from 'xlsx';
+import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { read, utils } from 'xlsx';
+import { Admin } from '../../Models/admin';
 import { AdminsService } from '../../Services/admins.service';
-
 
 @Component({
   selector: 'app-import-admin',
@@ -12,37 +13,101 @@ import { AdminsService } from '../../Services/admins.service';
 })
 export class ImportAdminComponent implements OnInit {
 
-  admin!:Admin[];
-  constructor(private router:Router,
-              private adminService:AdminsService) { }
+  newAdmin!: FormGroup;
+  admins!: any[];
+  subAdmin!: Subscription;
+  consoleError!: string;
+
+  constructor(
+    private fb: FormBuilder,
+    private adminService: AdminsService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
-  }
-  goback(){
-    this.router.navigate(['admins'])
+    this.newAdmin = this.fb.group({
+      id: [],
+      firstName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+      lastName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+      universityId: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      roles: this.fb.array([]),
+      password: [''],
+      specialization: ['', [Validators.required]],
+      enable: [true],
+      locked: [false]
+    });
   }
 
-  importAdmin(){
-  this.router.navigate(['admins']);
+  goback() {
+    this.router.navigate(['admins']);
   }
-  admins:any;
 
   handleImport($event: any) {
-      const files = $event.target.files;
-      if (files.length) {
-          const file = files[0];
-          const reader = new FileReader();
-          reader.onload = (event: any) => {
-              const wb = read(event.target.result);
-              const sheets = wb.SheetNames;
+    const files = $event.target.files;
+    if (files.length) {
+      const file = files[0];
+      const reader = new FileReader();
+      reader.onload = (event: any) => {
+        const wb = read(event.target.result);
+        const sheets = wb.SheetNames;
 
-              if (sheets.length) {
-                  const rows = utils.sheet_to_json(wb.Sheets[sheets[0]]);
-                  this.admins = rows;
-              }
-          }
-          reader.readAsArrayBuffer(file);
-      }
+        if (sheets.length) {
+          const rows: any = utils.sheet_to_json(wb.Sheets[sheets[0]]);
+          this.admins = rows;
+        }
+      };
+      reader.readAsArrayBuffer(file);
+    }
+  }
+
+  importAdmin() {
+    for (let admin of this.admins) {
+      const rolesArray: any[] = admin.roles.split(',');
+      const roles = rolesArray.map(role => ({ role }));
+      admin.roles = roles;
+
+      this.newAdmin.patchValue(admin);
+      this.subAdmin = this.adminService.addAdmin(this.newAdmin.value).subscribe({
+        next: (admin: Admin) => {
+        },
+        error: (err: Error) => {
+          this.consoleError = err.message;
+        }
+      });
+    }
+  }
+
+  get firstName() {
+    return this.newAdmin.get('firstName');
+  }
+
+  get lastName() {
+    return this.newAdmin.get('lastName');
+  }
+
+  get universityId() {
+    return this.newAdmin.get('universityId');
+  }
+
+  get email() {
+    return this.newAdmin.get('email');
+  }
+
+  get password() {
+    return this.newAdmin.get('password');
+  }
+
+  get roles() {
+    return this.newAdmin.get('roles') as FormArray;
+  }
+
+  get specialization() {
+    return this.newAdmin.get('specialization');
+  }
+
+  get enable() {
+    return this.newAdmin.get('enable');
   }
 
 }
