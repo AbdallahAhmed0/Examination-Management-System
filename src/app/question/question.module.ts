7de6ec3f126fa417fc913c1dae43d2ee75cdcd60
AbdallahAhmed0 <abdallahahmadadel@gmail.com>
2023-04-13 02:47:37 +0200

    fix error checked of radio button

--- src/app/question/question.module.ts
+++ src/app/question/question.module.ts
@@ -11,10 +11,10 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { TextQuestionsComponent } from './text-questions/text-questions.component'
 
-import { FroalaEditorModule, FroalaViewModule } from 'angular-froala-wysiwyg';
 import { EditQuestionsComponent } from './edit-questions/edit-questions.component';
 import { CodeQuestionComponent } from './code-question/code-question.component';
 import { TrueFalseQuestionComponent } from './true-false-question/true-false-question.component';
+import { FroalaEditorModule, FroalaViewModule } from 'angular-froala-wysiwyg';
 
 
 @NgModule({
