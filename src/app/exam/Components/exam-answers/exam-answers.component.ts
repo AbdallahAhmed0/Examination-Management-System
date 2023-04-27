5be80cf1edfbc29c79c77784df856b542d98f639
Mohamed <khedr66664@gmail.com>
2023-04-27 03:08:19 +0200

    add true false ans template & fix ans tags problem

--- src/app/exam/Components/exam-answers/exam-answers.component.ts
+++ src/app/exam/Components/exam-answers/exam-answers.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { tstarr } from './tst-data';
+import { DomSanitizer } from '@angular/platform-browser';
 @Component({
   selector: 'app-exam-answers',
   templateUrl: './exam-answers.component.html',
   styleUrls: ['./exam-answers.component.scss'],
 })
 export class ExamAnswersComponent implements OnInit {
-  constructor() {}
+  constructor(private sanitizer: DomSanitizer) {}
 
   ngOnInit(): void {}
   questions = tstarr;
+
+  // Security Step
+  // Sanitize the HTML content with the DomSanitizer service
+  sanitizeHtml(html: string): any {
+    return this.sanitizer.bypassSecurityTrustHtml(html);
+  }
 }
