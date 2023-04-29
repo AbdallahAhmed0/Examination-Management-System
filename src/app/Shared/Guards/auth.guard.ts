d9cd920bed05d39c1e2984b97e3a2ccb60b2cfeb
AbdallahAhmed0 <abdallahahmadadel@gmail.com>
2023-04-30 00:20:47 +0300

    edit service auth in login

--- src/app/Shared/Guards/auth.guard.ts
+++ src/app/Shared/Guards/auth.guard.ts
@@ -1,15 +1,24 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/login/Services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+
+  constructor(private authService: AuthService, private router: Router) { }
+
   canActivate(
-    route: ActivatedRouteSnapshot,
+next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+      if (this.authService) {
+        return true;
+      }
+      else {
+        this.router.navigate(['/login']);
+        return false;
+      }
   }
-  
 }
--- src/app/login/Services/auth.service.ts
+++ src/app/login/Services/auth.service.ts
@@ -3,7 +3,7 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../Model/user';
 import { Role } from '../../roles/Models/role';
-import { tap } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,27 +12,32 @@ export class AuthService {
 
   private apiUrl = 'http://localhost:3000/auth';
   private user!:User;
+  private loggedIn: boolean = false;
   private currentUserSubject: BehaviorSubject<User>;
   public currentUser$: Observable<User>;
 
   constructor(private http: HttpClient) {
     const currentUser = localStorage.getItem('currentUser');
     this.currentUserSubject = new BehaviorSubject<User>(currentUser ? JSON.parse(currentUser) : null);
-        this.currentUser$ = this.currentUserSubject.asObservable();
+    this.currentUser$ = this.currentUserSubject.asObservable();
   }
 
   public get currentUserValue(): User {
     return this.currentUserSubject.value;
   }
 
-  login(email: string, password: string): Observable<User> {
-    return this.http.post<User>(`${this.apiUrl}/login`, { email, password })
-      .pipe(
-        tap(user => {
+  login(username: string, password: string) {
+    return this.http.post<any>(`${this.apiUrl}/users/authenticate`, { username, password })
+      .pipe(map(user => {
+        // login successful if there's a jwt token in the response
+        if (user && user.token) {
+          // store user details and jwt token in local storage to keep user logged in between page refreshes
           localStorage.setItem('currentUser', JSON.stringify(user));
           this.currentUserSubject.next(user);
-        })
-      );
+        }
+
+        return user;
+      }));
   }
 
   logout(): void {
@@ -55,4 +60,17 @@ export class AuthService {
   getRoles(): Role[] {
     return this.currentUserValue?.roles;
   }
-}
+    // Check if user is logged in
+    isLoggedIn(): boolean {
+      const currentUser = localStorage.getItem('currentUser');
+      const user = JSON.parse(currentUser ? JSON.parse(currentUser) : null);
+      if (user && user.token) {
+        this.loggedIn = true;
+      } else {
+        this.loggedIn = false;
+      }
+      return this.loggedIn;
+    }
+  }
+
+
