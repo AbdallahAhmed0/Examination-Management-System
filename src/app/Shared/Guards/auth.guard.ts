d99cb7b088f0c374247de3066d00dec012abc93a
AbdallahAhmed0 <abdallahahmadadel@gmail.com>
2023-05-01 15:33:48 +0300

    edit Auth guard

--- src/app/Shared/Guards/auth.guard.ts
+++ src/app/Shared/Guards/auth.guard.ts
@@ -11,14 +11,24 @@ export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) { }
 
   canActivate(
-next: ActivatedRouteSnapshot,
+    route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      if (this.authService.isLoggedIn()) {
-        return true;
+      const user = this.authService.userValue;
+      if (user) {
+          // check if route is restricted by role
+          const { roles } = route.data;
+          if (roles && !roles.includes(user.role)) {
+              // role not authorized so redirect to home page
+              this.router.navigate(['/']);
+              return false;
+          }
+
+          // authorized so return true
+          return true;
       }
-      else {
-        this.router.navigate(['/login']);
-        return false;
-      }
+
+      // not logged in so redirect to login page with the return url
+      this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+      return false;
   }
 }
