79a666934a0775d0832dc855fd7a8140533e7069
AbdallahAhmed0 <abdallahahmadadel@gmail.com>
2023-05-01 15:21:09 +0300

    edit auth service

--- src/app/login/Services/auth.service.ts
+++ src/app/login/Services/auth.service.ts
@@ -4,73 +4,43 @@ import { HttpClient } from '@angular/common/http';
 import { User } from '../Model/user';
 import { Role } from '../../roles/Models/role';
 import { map, tap } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  private apiUrl = 'http://localhost:3000/auth';
-  private user!:User;
-  private loggedIn: boolean = false;
-  private currentUserSubject: BehaviorSubject<User>;
-  public currentUser$: Observable<User>;
+  private userSubject: BehaviorSubject<User | null>;
+  public user: Observable<User | null>;
 
-  constructor(private http: HttpClient) {
-    const currentUser = localStorage.getItem('currentUser');
-    this.currentUserSubject = new BehaviorSubject<User>(currentUser ? JSON.parse(currentUser) : null);
-    this.currentUser$ = this.currentUserSubject.asObservable();
+  constructor(
+      private router: Router,
+      private http: HttpClient
+  ) {
+      this.userSubject = new BehaviorSubject(JSON.parse(localStorage.getItem('user')!));
+      this.user = this.userSubject.asObservable();
   }
 
-  public get currentUserValue(): User {
-    return this.currentUserSubject.value;
+  public get userValue() {
+      return this.userSubject.value;
   }
 
   login(username: string, password: string) {
-    return this.http.post<any>(`${this.apiUrl}/users/authenticate`, { username, password })
-      .pipe(map(user => {
-        // login successful if there's a jwt token in the response
-        if (user && user.token) {
-          // store user details and jwt token in local storage to keep user logged in between page refreshes
-          localStorage.setItem('currentUser', JSON.stringify(user));
-          this.currentUserSubject.next(user);
-        }
-
-        return user;
-      }));
+      return this.http.post<any>(`${environment.APPURL}/users/authenticate`, { username, password })
+          .pipe(map(user => {
+              // store user details and jwt token in local storage to keep user logged in between page refreshes
+              localStorage.setItem('user', JSON.stringify(user));
+              this.userSubject.next(user);
+              return user;
+          }));
   }
 
-  logout(): void {
-    localStorage.removeItem('currentUser');
-    this.currentUserSubject.next(this.user);
+  logout() {
+      // remove user from local storage to log user out
+      localStorage.removeItem('user');
+      this.userSubject.next(null);
+      this.router.navigate(['/login']);
   }
-
-  hasRole(role: string): boolean {
-    return this.currentUserValue?.roles?.find(r => r.role === role) !== undefined;
-  }
-
-  isAdmin(): boolean {
-    return this.hasRole('admin');
-  }
-
-  isUser(): boolean {
-    return this.hasRole('user');
-  }
-
-  getRoles(): Role[] {
-    return this.currentUserValue?.roles;
-  }
-    // Check if user is logged in
-    isLoggedIn(): boolean {
-      const currentUser = localStorage.getItem('currentUser');
-      const user = JSON.parse(currentUser ? JSON.parse(currentUser) : null);
-      if (user && user.token) {
-        this.loggedIn = true;
-      } else {
-        this.loggedIn = false;
-      }
-      return this.loggedIn;
-    }
-  }
-
-
+}
