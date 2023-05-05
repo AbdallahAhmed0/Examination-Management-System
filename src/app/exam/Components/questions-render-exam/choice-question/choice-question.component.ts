da53fac2caffe51be3d688b4c441522d37e6ab3d
AbdallahAhmed0 <abdallahahmadadel@gmail.com>
2023-05-05 19:18:18 +0300

    show question choice

--- src/app/exam/Components/questions-render-exam/choice-question/choice-question.component.ts
+++ src/app/exam/Components/questions-render-exam/choice-question/choice-question.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Question } from './../../../../question/question';
 
 @Component({
   selector: 'app-choice-question',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./choice-question.component.scss']
 })
 export class ChoiceQuestionComponent implements OnInit {
-
-  constructor() { }
+@Input() question!:any;
+@Input() index!:number;
+  constructor(private sanitizer: DomSanitizer,) { }
 
   ngOnInit(): void {
   }
+    // Sanitize the HTML content with the DomSanitizer service
+    sanitizeHtml(html: string): any {
+      return this.sanitizer.bypassSecurityTrustHtml(html);
+    }
+
 
 }
