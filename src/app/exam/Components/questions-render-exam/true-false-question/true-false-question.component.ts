22fc5afbb4d6861e6a02d20661de5bc083e92729
AbdallahAhmed0 <abdallahahmadadel@gmail.com>
2023-05-06 06:33:36 +0300

    edit tue

--- src/app/exam/Components/questions-render-exam/true-false-question/true-false-question.component.ts
+++ src/app/exam/Components/questions-render-exam/true-false-question/true-false-question.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
@@ -9,11 +10,29 @@ import { DomSanitizer } from '@angular/platform-browser';
 export class TrueFalseQuestionComponent implements OnInit {
   @Input() question!:any;
   @Input() index!:number;
-    constructor(private sanitizer: DomSanitizer,) { }
 
-    ngOnInit(): void {
-    }
-      // Sanitize the HTML content with the DomSanitizer service
+  answerForm!: FormGroup;
+  answer:any;
+  constructor(private sanitizer: DomSanitizer,
+              private fb:FormBuilder) { }
+
+ngOnInit(): void {
+
+  this.answer = this.question.questionAnswers[0];
+this.answerForm = this.fb.group({
+questionId: [this.question.id],
+answersIds: this.fb.array([])
+});
+this.addAnswer();
+}
+get answers(): FormArray {
+return this.answerForm.get('answersIds') as FormArray;
+}
+addAnswer() {
+this.answers.push(new FormControl(''));
+}
+
+    // Sanitize the HTML content with the DomSanitizer service
       sanitizeHtml(html: string): any {
         return this.sanitizer.bypassSecurityTrustHtml(html);
       }
