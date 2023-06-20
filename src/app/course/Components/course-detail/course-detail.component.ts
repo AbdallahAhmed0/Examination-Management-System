f69d9079df1ecf7e9a0cafa051681cdd1275c653
AbdallahAhmed0 <abdallahahmadadel@gmail.com>
2023-06-21 02:49:43 +0300

    edit Course

--- src/app/course/Components/course-detail/course-detail.component.ts
+++ src/app/course/Components/course-detail/course-detail.component.ts
@@ -4,9 +4,7 @@ import { CourseService } from '../../course.service';
 import { Subscription } from 'rxjs';
 import { Course } from '../../course.model';
 import { Exam } from 'src/app/exam/Models/exam';
-import { ExamService } from 'src/app/exam/Services/exam.service';
 import { MatDialog } from '@angular/material/dialog';
-import { DialogeComponent } from '../../../Shared/material/dialog/dialog.component';
 
 
 @Component({
@@ -17,14 +15,12 @@ import { DialogeComponent } from '../../../Shared/material/dialog/dialog.compone
 export class CourseDetailComponent implements OnInit {
   subRoute ?:Subscription;
   id!: number;
-  course !: Course;
+  course ?: Course;
   Exams!:Exam[];
   admins !:any;
 
   constructor(private courseService:CourseService,
-              private activatedRoute:ActivatedRoute,
-              private examservice:ExamService,
-              public dialog: MatDialog) { }
+              private activatedRoute:ActivatedRoute) { }
 
   ngOnInit(): void {
     this.subRoute=this.activatedRoute.paramMap.subscribe((paramMap)=>{
@@ -32,34 +28,12 @@ export class CourseDetailComponent implements OnInit {
       this.courseService.getCourseById(this.id).subscribe(data=>{
         this.course=data
         this.admins=data.admins
-        console.log(data);
       })
       this.courseService.getExamsofCourse(this.id).subscribe(data=>{
         this.Exams=data
-        console.log(data);
-
       })
     })
   }
-  deleteExam(id :any){
-    const dialogRef = this.dialog.open(DialogeComponent, {
-      width: '400px',
-      height:'280px'
-      });
-
-    dialogRef.afterClosed().subscribe((result) => {
-      if (result === 'confirm') {
-        console.log(id);
-
-        this.examservice.deleteExam(id)
-       window.location.reload();
-
-      }
-
-      });
-
-
-  }
-
+  
 
 }
