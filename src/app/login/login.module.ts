76e0e1f2508a2176b87ddeb9350dc171c62c52cd
ahmed00Reda <ahmedmankk@gmail.com>
2023-06-23 02:17:19 +0300

    reset password

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -5,11 +5,15 @@ import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login-component/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from '../Shared/material/material.module';
+import { ForgetPasswordComponent } from './forget-password/forget-password.component';
+import { ForgetCodeComponent } from './forget-code/forget-code.component';
 
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    ForgetPasswordComponent,
+    ForgetCodeComponent
   ],
   imports: [
     CommonModule,
