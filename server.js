c2157d995eecc8abae6c4980ff8fb63be124b082
AbdallahAhmed0 <abdallahahmadadel@gmail.com>
2023-06-23 19:50:00 +0300

    deploy app

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+// const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+// Serve the static files from the dist directory
+app.use(express.static(path.join(__dirname, 'dist', 'examination-management-system')));
+
+// // Proxy configuration
+// app.use(
+//   '/api',
+//   createProxyMiddleware({
+//     target: 'https://www.ahbabkhatamelmorsalen.com',
+//     changeOrigin: true,
+//   })
+// );
+
+// Redirect all requests to the index.html file
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'dist', 'examination-management-system', 'index.html'));
+});
+
+// Start the app by listening on the default  port or 8080
+const port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log(`Server started on port ${port}`);
+});
