5cd0c1806a72ae32c22f497e6f4cd504b96b361e
AbdallahAhmed0 <abdallahahmadadel@gmail.com>
2023-07-06 02:39:55 +0300

    m

--- src/app/login/Interceptors/jwt.interceptor.ts
+++ src/app/login/Interceptors/jwt.interceptor.ts
@@ -2,24 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
-import { StorageService } from '../Services/storage.service';
+import { StorageServiceService } from '../Services/storage-service.service';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
-  constructor(private storageService: StorageService) { }
+    constructor(private storageService:StorageServiceService) { }
 
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    // add auth header with jwt if user is logged in and request is to api url
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        // add auth header with jwt if user is logged in and request is to api url
 
-<<<<<<< HEAD
-    const token = this.storageService.getToken();
-    const isLoggedIn = this.storageService.isLoggedIn();
-    const isApiUrl = request.url.startsWith('http://142.93.192.45:8088/api');
-    if (isLoggedIn) {
-      request = request.clone({
-        setHeaders: {
-          Authorization: `Bearer ${token.token}`
-=======
         const token = this.storageService.getToken();
         const isLoggedIn = this.storageService.isLoggedIn();
         const isApiUrl = request.url.startsWith('http://142.93.192.45:8087/api');
@@ -29,12 +20,7 @@ export class JwtInterceptor implements HttpInterceptor {
                     Authorization: `Bearer ${token.token}`
                 }
             });
-            console.log(token)
->>>>>>> 9095859eefeefd5ede17393ad6884d242ea8d017
         }
-      });
-      console.log(token)
+        return next.handle(request);
     }
-    return next.handle(request);
-  }
 }
