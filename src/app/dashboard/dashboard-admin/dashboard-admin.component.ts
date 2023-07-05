a58b706cb563326167413055c91bc240edcdbcf9
hosam shaker <hossamcosper@gmail.com>
2023-07-05 18:23:27 +0300

    Improving dashboard

--- src/app/dashboard/dashboard-admin/dashboard-admin.component.ts
+++ src/app/dashboard/dashboard-admin/dashboard-admin.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AdminsService } from 'src/app/admins/Services/admins.service';
+import { CourseService } from 'src/app/course/course.service';
+import { Exam } from 'src/app/exam/Models/exam';
+import { ExamService } from 'src/app/exam/Services/exam.service';
+import { StudentsService } from 'src/app/students/Services/students.service';
 
 @Component({
   selector: 'app-dashboard-admin',
@@ -6,7 +11,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard-admin.component.scss'],
 })
 export class DashboardAdminComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit(): void {}
+  totalExams = 0;
+  totalStudents = 0;
+  totalCourses = 0;
+  totalTeachers = 0;
+  constructor(private examService: ExamService,
+    private studentService: StudentsService,
+    private courseService: CourseService,
+    private teacherService: AdminsService) {
+
+  }
+
+  ngOnInit(): void {
+    this.examService.getAllExams().subscribe((exams: Exam[]) => {
+      this.totalExams = exams.length;
+    });
+    this.studentService.getAllStudents().subscribe((students: any[]) => {
+      this.totalStudents = students.length;
+    });
+    this.courseService.getAllCourses().subscribe((courses: any[]) => {
+      this.totalCourses = courses.length;
+    });
+    this.teacherService.getAllAdmins().subscribe((teachers: any[]) => {
+      this.totalTeachers = teachers.length;
+    });
+  }
 }
